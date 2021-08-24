17e41c42203a6c19706547a59dbcbc3fc382265c
James Wolfe <jr.wolfe@outlook.com>
2021-08-24 12:05:10 +0100

    added validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :toilet
   belongs_to :user
+
+  validates :start_time, presence: true
+  validates :end_time, presence: true, numericality: { only_integer: true }
 end
--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -1,3 +1,9 @@
 class Toilet < ApplicationRecord
   belongs_to :user
+  has_many :users, through: :bookings
+
+  validates :location, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :cost_per_minute, presence: true, numericality: { only_integer: true }
+  validates :access_info, presence: true
 end
