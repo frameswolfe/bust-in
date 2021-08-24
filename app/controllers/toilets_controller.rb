4c80b53e2aaae73fd9ffaa495526804359870455
James Wolfe <jr.wolfe@outlook.com>
2021-08-24 13:08:00 +0100

    created index and show actions

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -1,2 +1,15 @@
 class ToiletsController < ApplicationController
+  before_action :set_toilet, only: %i[show]
+
+  def index
+    @toilets = Toilet.all
+  end
+
+  def show; end
+
+  private
+
+  def set_toilet
+    @toilet = Toilet.find(params[:id])
+  end
 end
