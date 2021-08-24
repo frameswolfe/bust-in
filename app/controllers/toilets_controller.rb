21a2d2ef5dddc2049fa0ac60d6e9a88e7ae6be2b
James Wolfe <jr.wolfe@outlook.com>
2021-08-24 16:36:59 +0100

    toilet actions and views basic completed

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -1,5 +1,5 @@
 class ToiletsController < ApplicationController
-  before_action :set_toilet, only: %i[show]
+  before_action :set_toilet, only: %i[show edit update destroy]
 
   def index
     @toilets = Toilet.all
@@ -7,7 +7,40 @@ class ToiletsController < ApplicationController
 
   def show; end
 
+  def new
+    @toilet = Toilet.new
+  end
+
+  def create
+    @toilet = Toilet.new(toilet_params)
+    @toilet.user = current_user
+    if @toilet.save!
+      redirect_to toilet_path(@toilet)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @toilet.update(toilet_params)
+      redirect_to toilet_path(@toilet), notice: "Garden was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @toilet.destroy
+    redirect_to toilets_path, notice: "Garden was successfully destroyed."
+  end
+
   private
+
+  def toilet_params
+    params.require(:toilet).permit(:location, :description, :access_info, :cost_per_minute)
+  end
 
   def set_toilet
     @toilet = Toilet.find(params[:id])
