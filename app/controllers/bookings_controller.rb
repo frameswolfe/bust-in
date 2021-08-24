10983edeac519b4332a78a4fe0514fdf3adb36bf
Camilo Gil <cgilv6@gmail.com>
2021-08-24 16:03:53 +0100

    worked on bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,24 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @toilet = Toilet.find(params[:toilet_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @toilet = Toilet.find(params[:toilet_id])
+    redirect_to home_path(@toilet) if @booking.save
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :toilet_id)
+  end
+
+  # def destroy
+  #   @booking = Booking.find(params[:id])
+  #   @booking.destroy
+  #   redirect_to home_path(@booking.list)
+  # end
 end
