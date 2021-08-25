da65f214017bc965e507f58f9cb875c990bdc36d
Camilo Gil <cgilv6@gmail.com>
2021-08-25 11:19:28 +0100

    g-controler-bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,8 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
   def new
     @booking = Booking.new
     @toilet = Toilet.find(params[:toilet_id])
@@ -7,13 +11,17 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @toilet = Toilet.find(params[:toilet_id])
-    redirect_to home_path(@toilet) if @booking.save
+    if @booking.save
+      redirect_to toilets_path
+    else
+      render :new
+    end
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_time, :toilet_id)
+    params.require(:booking).permit(:start_time, :end_time)
   end
 
   # def destroy
