8fa96b4d7488ad7791e64c92050013af152dc47c
Katherine Wise <k06kw02@gmail.com>
2021-08-26 12:37:31 +0100

    added cloudinary image upload and placeholder

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -2,6 +2,7 @@ class Toilet < ApplicationRecord
   belongs_to :user
   has_many :users, through: :bookings
   has_many :bookings, dependent: :destroy
+  has_one_attached :photo
 
   validates :location, presence: true, uniqueness: true
   validates :description, presence: true
